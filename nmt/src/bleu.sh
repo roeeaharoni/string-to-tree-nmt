bfae3ff2047ca5c9d4b7ed4a5bc13563c03bf74f
roeeaharoni <roee.aharoni@gmail.com>
2016-12-31 15:38:53 +0200

    test files

--- nmt/src/bleu.sh
+++ nmt/src/bleu.sh
@@ -12,17 +12,17 @@ mosesdecoder=$base_path/git/mosesdecoder
 device=gpu
 
 # model prefix
-prefix=$base_path/git/research/nmt/models/de_en_stt_model.npz
+model_prefix=$base_path/git/research/nmt/models/de_en_stt_model.npz
 
 # dev (source) and ref files
-dev=$base_path/git/research/nmt/data/WMT16/en-de/dev/newstest2015-deen-src.tok.true.de
-ref=$base_path/git/research/nmt/data/WMT16/en-de/dev/newstest2015-deen-ref.en
+dev=$base_path/git/research/nmt/data/WMT16/en-de/dev/newstest2015-deen-src.tok.true.de.100
+ref=$base_path/git/research/nmt/data/WMT16/en-de/dev/newstest2015-deen-ref.en.100
 
 dev_target=$base_path/git/research/nmt/models/newstest2015-deen-src.tok.true.de.output.sents.dev.postprocessed.dev
 
 # get BLEU
-BEST=`cat ${prefix}_best_bleu` || echo 0
-$mosesdecoder/scripts/generic/multi-bleu.perl $ref < $dev_target >> ${prefix}_bleu_scores
+BEST=`cat ${model_prefix}_best_bleu` || echo 0
+$mosesdecoder/scripts/generic/multi-bleu.perl $ref < $dev_target >> ${model_prefix}_bleu_scores
 BLEU=`$mosesdecoder/scripts/generic/multi-bleu.perl $ref < $dev_target | cut -f 3 -d ' ' | cut -f 1 -d ','`
 BETTER=`echo "$BLEU > $BEST" | bc`
 
@@ -31,6 +31,6 @@ echo "BLEU = $BLEU"
 # save model with highest BLEU
 if [ "$BETTER" = "1" ]; then
   echo "new best; saving"
-  echo $BLEU > ${prefix}_best_bleu
-  cp ${prefix}.dev.npz ${prefix}.npz.best_bleu
+  echo $BLEU > ${model_prefix}_best_bleu
+  cp ${model_prefix}.dev.npz ${model_prefix}.npz.best_bleu
 fi
