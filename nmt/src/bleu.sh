94144a4b219571aeeab61f0e83c983862d954e37
roeeaharoni <roee.aharoni@gmail.com>
2016-12-31 18:40:25 +0200

    test files

--- nmt/src/bleu.sh
+++ nmt/src/bleu.sh
@@ -31,23 +31,14 @@ echo 'wrote current BLEU'
 BLEU=`$mosesdecoder/scripts/generic/multi-bleu.perl $ref < $dev_target | cut -f 3 -d ' ' | cut -f 1 -d ','`
 echo 'extracted current BLEU'
 
-echo "$BLEU"
-echo "$BEST"
 
 echo 'check if better...'
-if [ "$BLEU" > "$BEST" ]; then
-    BETTER="1"
+if [ "$BLEU" -gt "$BEST" ]; then
+    echo "new best; saving"
+    echo $BLEU > ${model_prefix}_best_bleu
+    cp ${model_prefix}.dev.npz ${model_prefix}.npz.best_bleu
 else
-    BETTER="0"
-fi
-echo "checked if better: $BETTER"
-
-echo "BLEU = $BLEU"
-
-echo 'checking if to save...'
-# save model with highest BLEU
-if [ "$BETTER" = "1" ]; then
-  echo "new best; saving"
-  echo $BLEU > ${model_prefix}_best_bleu
-  cp ${model_prefix}.dev.npz ${model_prefix}.npz.best_bleu
+    echo "current: $BLEU"
+    echo "best: $BEST"
+    echo "no improvement"
 fi
