e6e9a3f374946ede72a6baa8acccac75d39fb722
roeeaharoni <roee.aharoni@gmail.com>
2016-12-31 15:24:12 +0200

    test files

--- nmt/src/bleu.sh
+++ nmt/src/bleu.sh
@@ -18,7 +18,7 @@ prefix=$base_path/git/research/nmt/models/de_en_stt_model.npz
 dev=$base_path/git/research/nmt/data/WMT16/en-de/dev/newstest2015-deen-src.tok.true.de
 ref=$base_path/git/research/nmt/data/WMT16/en-de/dev/newstest2015-deen-ref.en
 
-dev_target = $base_path/git/research/nmt/models/newstest2015-deen-src.tok.true.de.output.sents.dev.postprocessed.dev
+dev_target=$base_path/git/research/nmt/models/newstest2015-deen-src.tok.true.de.output.sents.dev.postprocessed.dev
 
 # get BLEU
 BEST=`cat ${prefix}_best_bleu || echo 0`
