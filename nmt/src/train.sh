aef7ae2830d301fb8b61866024f4c6d2f7a4f70f
roeeaharoni <roee.aharoni@gmail.com>
2017-01-03 18:12:01 +0200

    test files

--- nmt/src/train.sh
+++ nmt/src/train.sh
@@ -11,12 +11,18 @@ THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu3,lib.cnmem=1 $nematus/nemat
 $base_path/git/research/nmt/data/WMT16/en-de/train/corpus.parallel.tok.en.parsed2.final.true.bped.final \
 --dictionaries $base_path/git/research/nmt/data/WMT16/en-de/train/corpus.parallel.tok.true.de.bpe.json \
 $base_path/git/research/nmt/data/WMT16/en-de/train/corpus.parallel.tok.en.parsed2.final.true.bped.final.json \
---model $base_path/git/research/nmt/models/de_en_stt_model.npz --saveFreq 30000 --dim_word 500 \
+--model $base_path/git/research/nmt/models/de_en_stt_model.npz \
+--saveFreq 30000 \
+--validFreq 10000 \
+--dim_word 500 \
 --dim 1024 --valid_datasets $base_path/git/research/nmt/data/WMT16/en-de/dev/newstest2015-deen-src.tok.true.de.bpe \
 $base_path/git/research/nmt/data/WMT16/en-de/dev/newstest2015-deen-ref.tok.true.parsed.en.bped \
---external_validation_script $base_path/git/research/src/validate_trees.sh \
+--external_validation_script $base_path/git/research/nmt/src/validate_trees.sh \
 --optimizer adadelta \
 --alpha_c 0.0 \
 --clip_c 1.0 \
 --maxlen 150 \
+--batch_size 20 \
+--valid_batch_size 20 \
 --reload
+
