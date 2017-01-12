bcd09e13c8906c601a987f78d0d2a0b1f8f07a8b
Roee Aharoni <aharonr6@nlp2.local>
2017-01-12 16:55:16 +0200

    fixed validate best to point correct path

--- nmt/src/validate_best_trees.py
+++ nmt/src/validate_best_trees.py
@@ -6,14 +6,14 @@ def main():
     print 'validating trees...'
     base_path = '/home/nlp/aharonr6'
     nematus = base_path + '/git/nematus'
-    model_prefix = base_path + '/git/research/nmt/models/de_en_stt_no_bpe_source/de_en_stt_model.npz.npz.best_bleu'
+    model_prefix = base_path + '/git/research/nmt/models/de_en_stt/de_en_stt_model.npz.npz.best_bleu'
     dev_src = base_path + '/git/research/nmt/data/WMT16/de-en/dev/newstest2015-deen-src.tok.true.de.bpe'
     dev_target = base_path + '/git/research/nmt/models/de_en_stt/newstest2015-deen-src.tok.true.de.bpe.output.trees.dev.best'
     dev_target_sents = base_path + '/git/research/nmt/models/de_en_stt/newstest2015-deen-src.tok.true.de.bpe.output.sents.dev.best'
     alignments_path = base_path + '/git/research/nmt/models/de_en_stt/best_dev_alignments.txt'
 
     # decode: k - beam size, n - normalize scores by length, p - processes
-    decode_command = 'THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu0,lib.cnmem=0.09,on_unused_input=warn python {}/nematus/translate.py \
+    decode_command = 'THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu2,lib.cnmem=0.09,on_unused_input=warn python {}/nematus/translate.py \
      -m {} \
      -i {} \
      -o {} \
