3b1f3cba4078cbd00401a0cbc24b72dcb020311d
Roee Aharoni <aharonr6@nlp1.local>
2017-01-12 13:58:03 +0200

    changed validation to work on gpu1, 1 process

--- nmt/src/validate_trees.py
+++ nmt/src/validate_trees.py
@@ -14,12 +14,12 @@ def main():
     alignments_path = base_path + '/git/research/nmt/models/de_en_stt/dev_alignments.txt'
 
     # decode: k - beam size, n - normalize scores by length, p - processes
-    decode_command = 'THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu0,lib.cnmem=0.09,on_unused_input=warn python {}/nematus/translate.py \
+    decode_command = 'THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu1,lib.cnmem=0.09,on_unused_input=warn python {}/nematus/translate.py \
      -m {}.dev.npz \
      -i {} \
      -o {} \
      -a {} \
-     -k 12 -n -p 5 -v'.format(nematus, model_prefix, dev_src, dev_target, alignments_path)
+     -k 12 -n -p 1 -v'.format(nematus, model_prefix, dev_src, dev_target, alignments_path)
     os.system(decode_command)
 
     print 'finished translating {}'.format(dev_src)
