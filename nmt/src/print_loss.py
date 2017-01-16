12402890ddbfea3c67e8648f659c440391bf7b24
Roee Aharoni <aharonr6@nlp1.local>
2017-01-16 19:09:51 +0200

    added print_loss.py

---
+++ nmt/src/print_loss.py
@@ -0,0 +1,8 @@
+import numpy
+
+saveto = '/home/nlp/aharonr6/git/research/nmt/models/en_de_tts/en_de_tts_model.npz.dev.npz'
+rmodel = numpy.load(saveto)
+history_errs = list(rmodel['history_errs'])
+for i,loss in enumerate(history_errs):
+	print i, loss
+
