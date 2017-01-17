30048b7a2e85141b47da13cd806c411550389b31
Roee Aharoni <aharonr6@nlp1.local>
2017-01-17 10:41:19 +0200

    added permissions to scripts, added new best files

--- nmt/src/print_loss.py
+++ nmt/src/print_loss.py
@@ -1,6 +1,7 @@
 import numpy
 
-saveto = '/home/nlp/aharonr6/git/research/nmt/models/en_de_tts/en_de_tts_model.npz.dev.npz'
+#saveto = '/home/nlp/aharonr6/git/research/nmt/models/en_de_tts/en_de_tts_model.npz.dev.npz'
+saveto = '/home/nlp/aharonr6/git/research/nmt/models/de_en_stt/de_en_stt_model.npz.dev.npz'
 rmodel = numpy.load(saveto)
 history_errs = list(rmodel['history_errs'])
 for i,loss in enumerate(history_errs):
