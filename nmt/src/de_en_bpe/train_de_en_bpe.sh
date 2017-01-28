d741ff749b55313c670f4921d1b6f8c4d10d48d9
roeeaharoni <roee.aharoni@gmail.com>
2017-01-28 17:40:08 +0200

    gpu0

--- nmt/src/de_en_bpe/train_de_en_bpe.sh
+++ nmt/src/de_en_bpe/train_de_en_bpe.sh
@@ -6,7 +6,7 @@ base_path=/home/nlp/aharonr6
 nematus=$base_path/git/nematus
 
 # train model with nematus
-THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu3,lib.cnmem=0.8 $nematus/nematus/nmt.py \
+THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu0,lib.cnmem=0.8 $nematus/nematus/nmt.py \
 --datasets $base_path/git/research/nmt/data/WMT16/de-en/train/corpus.parallel.tok.true.de.bpe \
 $base_path/git/research/nmt/data/WMT16/de-en/train/corpus.parallel.tok.true.en.bpe \
 --dictionaries $base_path/git/research/nmt/data/WMT16/de-en/train/corpus.parallel.tok.true.de.bpe.json \
