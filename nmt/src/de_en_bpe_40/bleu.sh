0ba5a1da4fd69b87fe860ef8d60734384c578d0e
roeeaharoni <roee.aharoni@gmail.com>
2017-01-30 22:49:01 +0200

    added scripts for bpe40 model

---
+++ nmt/src/de_en_bpe_40/bleu.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+base_path=/home/nlp/aharonr6
+
+# path to nematus ( https://www.github.com/rsennrich/nematus )
+nematus=$base_path/git/nematus
+
+# path to moses decoder: https://github.com/moses-smt/mosesdecoder
+mosesdecoder=$base_path/git/mosesdecoder
+
+# model prefix
+model_prefix=$base_path/git/research/nmt/models/de_en_bpe_40/de_en_bpe_40_model.npz
+
+# dev ref file
+ref=$base_path/git/research/nmt/data/WMT16/de-en/dev/newstest2015-deen-ref.en
+
+# dev predictions file
+dev_target=$base_path/git/research/nmt/models/de_en_bpe_40/newstest2015-deen-src.tok.true.de.bpe.output.dev.postprocessed
+
+dev_alignments=$base_path/git/research/nmt/models/de_en_bpe_40/dev_alignments.txt
+
+# get prev best BLEU
+BEST=`cat ${model_prefix}_best_bleu || echo 0`
+echo "prev best BLEU: $BEST"
+
+# write current BLEU
+$mosesdecoder/scripts/generic/multi-bleu.perl $ref < $dev_target >> ${model_prefix}_bleu_scores
+echo "wrote current BLEU to: ${model_prefix}_bleu_scores"
+
+# extract current BLEU
+BLEU=`$mosesdecoder/scripts/generic/multi-bleu.perl $ref < $dev_target | cut -f 3 -d ' ' | cut -f 1 -d ','`
+echo "extracted current BLEU: $BLEU"
+
+# check if to save new model as best model
+echo 'check if better...'
+echo "current: $BLEU"
+echo "best: $BEST"
+
+if (( $(echo "$BLEU > $BEST" |bc -l) )); then
+    echo "new best; saving"
+    echo $BLEU > ${model_prefix}_best_bleu
+    cp ${model_prefix}.dev.npz ${model_prefix}.npz.best_bleu
+    cp ${dev_target} ${dev_target}.best
+    cp ${dev_alignments} ${dev_alignments}.best
+    echo "improved! saved best in: ${model_prefix}.npz.best_bleu"
+else
+    echo "no improvement"
+fi
---
+++ nmt/src/de_en_bpe_40/train_de_en_bpe.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+base_path=/home/nlp/aharonr6
+
+# path to nematus ( https://www.github.com/rsennrich/nematus )
+nematus=$base_path/git/nematus
+
+# train model with nematus
+THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu0,lib.cnmem=0.8 $nematus/nematus/nmt.py \
+--datasets $base_path/git/research/nmt/data/WMT16/de-en/train/corpus.parallel.tok.true.de.bpe.copy40 \
+$base_path/git/research/nmt/data/WMT16/de-en/train/corpus.parallel.tok.true.en.bpe.copy40 \
+--dictionaries $base_path/git/research/nmt/data/WMT16/de-en/train/corpus.parallel.tok.true.de.bpe.json \
+$base_path/git/research/nmt/data/WMT16/de-en/train/corpus.parallel.tok.en.parsed2.final.true.bped.final.json \
+--model $base_path/git/research/nmt/models/de_en_bpe_40/de_en_bpe_40_model.npz \
+--saveFreq 30000 \
+--validFreq 10000 \
+--dim_word 500 \
+--dim 1024 \
+--valid_datasets $base_path/git/research/nmt/data/WMT16/de-en/dev/newstest2015-deen-src.tok.true.de.bpe \
+$base_path/git/research/nmt/data/WMT16/de-en/dev/newstest2015-deen-ref.tok.true.en.bpe \
+--external_validation_script $base_path/git/research/nmt/src/de_en_bpe_40/validate.sh \
+--optimizer adadelta \
+--alpha_c 0.0 \
+--clip_c 1.0 \
+--maxlen 50 \
+--batch_size 40 \
+--valid_batch_size 10 \
+--reload
