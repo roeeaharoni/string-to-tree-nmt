a36c3e7c61bac6d3031f144923eb2bf7109a6e27
roeeaharoni <roee.aharoni@gmail.com>
2017-02-10 13:03:17 +0200

    debug train

--- nmt/src/debug_train_data.py
+++ nmt/src/debug_train_data.py
@@ -43,7 +43,7 @@ def main():
 
                 # check if same
                 if en_bpe_line.strip() != sent:
-                    print 'diff in trg files:\nbpe:{}\nstt:{}\n'.format(en_bpe_line, sent)
+                    print u'diff in trg files:\nbpe:{}\nstt:{}\ntree:{}\n'.format(en_bpe_line, sent,en_stt_line)
 
 
 
