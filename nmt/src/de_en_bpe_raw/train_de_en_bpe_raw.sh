1ab1f4defeacdcf05ac86fb31e030f670a606c88
roeeaharoni <roee.aharoni@gmail.com>
2017-02-14 16:55:49 +0200

    added scripts and dev/test data for wmt16 baseline training

---
+++ nmt/src/de_en_bpe_raw/train_de_en_bpe_raw.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+base_path=/home/nlp/aharonr6
+
+# path to nematus ( https://www.github.com/rsennrich/nematus )
+nematus=$base_path/git/nematus
+
+# train model with nematus
+THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu0,lib.cnmem=0.8 $nematus/nematus/nmt.py \
+--datasets $base_path/git/research/nmt/data/WMT16/de-en-raw/train/wmt16.train.tok.clean.true.bpe.de \
+$base_path/git/research/nmt/data/WMT16/de-en-raw/train/wmt16.train.tok.clean.true.bpe.en \
+--dictionaries $base_path/git/research/nmt/data/WMT16/de-en-raw/train/wmt16.train.tok.clean.true.bpe.de.json \
+$base_path/git/research/nmt/data/WMT16/de-en-raw/train/wmt16.train.tok.clean.true.bpe.en.json \
+--model $base_path/git/research/nmt/models/de_en_bpe_raw/de_en_bpe_raw_model.npz \
+--saveFreq 30000 \
+--validFreq 10000 \
+--dim_word 500 \
+--dim 1024 \
+--valid_datasets $base_path/git/research/nmt/data/WMT16/de-en-raw/dev/newstest-2013-2014-deen.tok.clean.true.bpe.de \
+$base_path/git/research/nmt/data/WMT16/de-en-raw/dev/newstest-2013-2014-deen.tok.clean.true.bpe.en \
+--external_validation_script $base_path/git/research/nmt/src/de_en_bpe_raw/validate.sh \
+--optimizer adadelta \
+--lrate 0.0001 \
+--alpha_c 0.0 \
+--clip_c 1.0 \
+--dropout_embedding 0 \
+--dropout_hidden 0 \
+--dropout_source 0 \
+--dropout_target 0 \
+--maxlen 50 \
+--batch_size 80 \
+--valid_batch_size 80 \
+--reload
---
+++ nmt/src/de_en_bpe_raw/validate.py
@@ -0,0 +1,41 @@
+import os
+import codecs
+
+def main():
+    print 'validating...'
+
+    base_path = '/home/nlp/aharonr6'
+
+    nematus = base_path + '/git/nematus'
+
+    model_prefix = base_path + '/git/research/nmt/models/de_en_bpe_raw/de_en_bpe_raw_model.npz'
+
+    dev_src = base_path + '/git/research/nmt/data/WMT16/de-en-raw/dev/newstest-2013-2014-deen.tok.clean.true.bpe.de'
+
+    dev_target = base_path+ '/git/research/nmt/models/de_en_bpe_raw/newstest-2013-2014-deen.tok.clean.true.bpe.de.output.dev'
+
+    alignments_path = base_path + '/git/research/nmt/models/de_en_bpe_raw/dev_alignments.txt'
+
+    # decode: k - beam size, n - normalize scores by length, p - processes
+    decode_command = 'THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu0,lib.cnmem=0.09,on_unused_input=warn python {}/nematus/translate.py \
+     -m {}.dev.npz \
+     -i {} \
+     -o {} \
+     -a {} \
+     -k 12 -n -p 5 -v'.format(nematus, model_prefix, dev_src, dev_target, alignments_path)
+    os.system(decode_command)
+
+    print 'finished translating {}'.format(dev_src)
+
+    # postprocess predictions (remove bpe, de-truecase)
+    postprocess_command = base_path + '/git/research/nmt/src/postprocess-en.sh < {} > {}.postprocessed'.format(dev_target, dev_target)
+    os.system(postprocess_command)
+    print 'postprocessed (de-bped, de-truecase) {} into {}.postprocessed'.format(dev_target, dev_target)
+
+    # get current BLEU, compare to last best model, save as best if improved
+    bleu_command = './bleu.sh'
+    os.system(bleu_command)
+
+
+if __name__ == '__main__':
+    main()
