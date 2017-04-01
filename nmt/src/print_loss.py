2cc72e134c1f6a1b6f9c0b004150fe6cc48b9f35
roeeaharoni <roee.aharoni@gmail.com>
2017-04-01 12:38:46 +0300

    made print_loss get an argument

--- nmt/src/print_loss.py
+++ nmt/src/print_loss.py
@@ -1,9 +1,14 @@
 import numpy
+import sys
 
-#saveto = '/home/nlp/aharonr6/git/research/nmt/models/en_de_tts/en_de_tts_model.npz.dev.npz'
-saveto = '/home/nlp/aharonr6/git/research/nmt/models/de_en_stt/de_en_stt_model.npz.dev.npz'
-rmodel = numpy.load(saveto)
-history_errs = list(rmodel['history_errs'])
-for i,loss in enumerate(history_errs):
-	print i, loss
+def main(path):
+    # path = '/home/nlp/aharonr6/git/research/nmt/models/en_de_tts/en_de_tts_model.npz.dev.npz'
+    # path = '/home/nlp/aharonr6/git/research/nmt/models/de_en_stt/de_en_stt_model.npz.dev.npz'
+    rmodel = numpy.load(path)
+    history_errs = list(rmodel['history_errs'])
+    for i,loss in enumerate(history_errs):
+        print i, loss
+
+if __name__ == '__main__':
+    main(sys.argv[1])
 
