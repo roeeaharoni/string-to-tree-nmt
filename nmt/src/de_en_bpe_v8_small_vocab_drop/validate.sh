5f6d60dbdf8b1b57b243347fef4d36236ab5b08b
roeeaharoni <roee.aharoni@gmail.com>
2017-04-10 18:45:33 +0300

    fixed model name in validate

--- nmt/src/de_en_bpe_v8_small_vocab_drop/validate.sh
+++ nmt/src/de_en_bpe_v8_small_vocab_drop/validate.sh
@@ -2,4 +2,4 @@
 
 base_path=/home/nlp/aharonr6
 export PYTHONPATH=$base_path/git/research/nmt
-python $base_path/git/research/nmt/src/de_en_bpe_v8_small_vocab/validate.py
+python $base_path/git/research/nmt/src/de_en_bpe_v8_small_vocab_drop/validate.py
