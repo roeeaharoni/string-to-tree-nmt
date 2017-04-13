292ebfdf6802d2c297e613eaacee70e9296b2a81
roeeaharoni <roee.aharoni@gmail.com>
2017-04-13 10:31:11 +0300

    added nolimit model for stt v8

---
+++ nmt/src/de_en_stt_v8_small_vocab_drop_nolimit/validate_trees.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+base_path=/home/nlp/aharonr6
+export PYTHONPATH=$base_path/git/research/nmt
+python $base_path/git/research/nmt/src/de_en_stt_v8_small_vocab_drop_nolimit/validate_trees.py
